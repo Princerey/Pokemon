39fd0e2a74f063c5acb76c62848f742bbae76b30
Princerey <rohkumar0126@gmail.com>
2022-11-06 13:52:50 +0530

    Added home animation and PokeBattle

--- Pokemon Card Generator/cursor1.js
+++ Pokemon Card Generator/cursor1.js
@@ -1,3 +1,4 @@
+$(window).on('mousemove', moveCircle29);
 var $circle9 = $('.circle');
 
 function moveCircle29(m) {
@@ -15,6 +16,37 @@ $( ".submit" ).click(function() {
             wrapper.addClass( "checked" );
             setTimeout(function(){
                 wrapper.removeClass( "checked" );
-            }, 8000);
+            }, 9000);
        }
-});
+});  // add class on click javascript
+  function addClass() {
+    // Get the paragraph element
+    const element = document.getElementById("id9");
+    // Add a class to the paragraph
+    element.className = "beautify line-1 anim-typewriter ";
+    const element9 = document.getElementById("id10");
+    // Add a class to the paragraph
+    element9.className = "beautify1 line-2 anim-typewriter1";
+    const element1 = document.getElementById("card")
+    element1.classList.add("blur");
+    const element2 = document.getElementById("card1")
+    element2.classList.add("blur");
+    
+    const element3 = document.getElementById("12")
+    element3.classList.add("bg");
+    element3.classList.add("win1");
+
+ 
+    const element4 = document.getElementById("13")
+    element4.classList.add("bg");
+    element4.classList.add("win1");
+      
+    const element5 = document.getElementById("button-wrapper")
+    element5.classList.add("nonbt");
+    const element6= document.getElementById("10");
+    element6.className = "b";
+    const element8= document.getElementById("100");
+    element8.className = "b1";
+    
+  }
+
